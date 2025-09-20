11caeac3b8635b2718a71068c04698b66eeda7a0
noorhanspal <deepnoor977@gmail.com>
2025-09-20 23:26:30 +0530

    Added greet page and updated templates

--- app.py
+++ app.py
@@ -1,25 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-#home page
 @app.route('/')
 def home():
-    name = 'Noor'
-    items = ["Python", "Flask","HTML","CSS"]
-    return render_template('home.html',user=name,skills=items)
+    return render_template('home.html')
 
-# about page
-@app.route('/about')
-def about():
-    return "this is about page"
-
-# contact page
-@app.route('/contact')
-def contact():
-    @app.route('/contact')
-    def contact():
-        return "this is contact page"
+@app.route('/greet', methods=['GET', 'POST'])
+def greet():
+    if request.method == 'POST':
+        username = request.form.get('username')  # Form se data lena
+        return f"Hello, {username}! 🎉"
+    return render_template('greet.html')  # Form dikhana
 
 if __name__ == '__main__':
     app.run(debug=True)
